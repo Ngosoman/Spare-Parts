a1519191228ba22ba6458dc7b7e0b293b97be876
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-16 17:46:44 +0300

    Implemented the edit profile function

---
+++ Frontend/src/pages/Buyer/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">My Profile</h2>
+      <p>Edit profile</p>
+    </div>
+  );
+};
+
+export default Profile;
