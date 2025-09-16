6290ed557cb363b357dae5ae686845fdfe740f07
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-16 19:02:39 +0300

    Updated browsing products to allow link to button show complete button order

--- Frontend/src/pages/Buyer/BrowseProducts.jsx
+++ Frontend/src/pages/Buyer/BrowseProducts.jsx
@@ -29,12 +29,12 @@ const BrowseProducts = () => {
               <h3 className="font-bold text-lg">{product.name}</h3>
               <p className="text-gray-600">{product.description}</p>
               <p className="text-blue-600 font-semibold mt-2">Ksh {product.price}</p>
-              <button
-                onClick={() => handleBuyNow(product)}
-                className="mt-3 bg-blue-500 text-white px-4 py-2 rounded-lg"
-              >
-                Buy Now
-              </button>
+              <Link to={`/buyer/checkout/${p.id}`}>
+                <button className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700">
+                    Buy Now
+                </button>
+                </Link>
+
             </div>
           ))}
         </div>
