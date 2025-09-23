50cbfc27d996c2fb6acb946a514e986cf1ec2485
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-23 09:29:12 +0300

    Updated Database settings in backend in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spareparts_db',
+        'USER': 'postgres',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
