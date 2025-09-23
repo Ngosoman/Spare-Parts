0f08c809950d67ae5ec4f984915cfd3c7d05f483
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-23 16:59:31 +0300

    Added Routes to backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from products.views import ProductViewSet
+
+router = routers.DefaultRouter()
+router.register(r'products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
