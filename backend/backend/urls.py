0900177426b8783f8740464137dcc6284f532ff1
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 13:29:36 +0300

    Updated tokens in urls to connect to the end points of Authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,6 +20,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from products.views import ProductViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -27,4 +32,6 @@ router.register(r'products', ProductViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
