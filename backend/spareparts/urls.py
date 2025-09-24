0786ea172e23ce98d555e4ee48e5d334e0275b60
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 09:36:07 +0300

    Linked Api into my main urls

--- backend/spareparts/urls.py
+++ backend/spareparts/urls.py
@@ -1,10 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import SparePartViewSet
-
-router = DefaultRouter()
-router.register(r'spareparts', SparePartViewSet)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('products.urls')),  # link app products
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/spareparts/', include('spareparts.urls')),  # link app spareparts
+    
 ]
