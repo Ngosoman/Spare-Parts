2c04e4dbbb986a323e217f707f9a3d28b251cdf8
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 13:35:17 +0300

    Added Permissions to orders API

--- backend/products/views.py
+++ backend/products/views.py
@@ -22,6 +22,7 @@ class ProductViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
     
 
 
