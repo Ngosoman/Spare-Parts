d2a16184a8e157bedc1fd8c1fe31a0e8f7ff944b
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 13:15:46 +0300

    installed django filter

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,19 +1,18 @@
-from django.db import models
-from django.utils import timezone
-
-from django.db import models
+# from django.utils import timezone
+# from django.db import models
+# from django.contrib.auth.models import User
 from django.contrib.auth.models import User
-from products.models import Product
+from django.db import models
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
 
 
 
