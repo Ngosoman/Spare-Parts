003953073b7b3ed6c3949f70e9fb7329155b8e46
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 09:38:39 +0300

    Updated comments in mains urls

--- backend/spareparts/urls.py
+++ backend/spareparts/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('products.urls')),  # link app products
+    path('api/', include('products.urls')),  #  app products
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/spareparts/', include('spareparts.urls')),  # link app spareparts
-    
+    path('api/spareparts/', include('spareparts.urls')),  #  app spareparts
+
 ]
