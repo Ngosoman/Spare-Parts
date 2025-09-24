ec3cda08cae0d567f7ae010fee18a462c99426e3
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 08:57:02 +0300

    Refactor Product model to set created_at field default to timezone.now and remove stock field

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
