f7dfcaae61936070463d2ad896a18d22edfca189
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 11:57:03 +0300

    connected order's class to serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from .models import Product
+from rest_framework import serializers
+from .models import Order
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        read_only_fields = ['buyer', 'total_price', 'status']
