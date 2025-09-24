04921b32f0d038f67e9c013ca69522dd4893ff5a
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 09:47:27 +0300

    added permission

--- backend/products/views.py
+++ backend/products/views.py
@@ -5,10 +5,13 @@ from rest_framework import filters
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
 
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'description']
