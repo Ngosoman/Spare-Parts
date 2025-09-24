82771ec0382a808f046ef9ab2a32b0f1224eecf3
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-24 08:12:39 +0300

    Implemented app's sparepart's views to connect with serializers and models

--- backend/spareparts/views.py
+++ backend/spareparts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import SparePart
+from .serializers import SparePartSerializer
+
+class SparePartViewSet(viewsets.ModelViewSet):
+    queryset = SparePart.objects.all()
+    serializer_class = SparePartSerializer
