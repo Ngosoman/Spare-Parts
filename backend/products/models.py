a192dd7a7bc5033c1cdc8c11d8cc7f62c0034782
Tom Mwambingu <[tomchom30@gmail.com]>
2025-09-25 09:28:53 +0300

    Intergrated mpesa STK and made migrations

--- backend/products/models.py
+++ backend/products/models.py
@@ -30,3 +30,22 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by {self.buyer.username}"
+
+
+
+#Mpesa Transaction Model
+class MpesaRequest(models.Model):
+    phone_number = models.CharField(max_length=15)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    account_reference = models.CharField(max_length=50)
+    transaction = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class MpesaResonse(models.Model):
+    request = models.ForeignKey(MpesaRequest, on_delete=models.CASCADE)
+    merchant_request_id = models.CharField(max_length=255)
+    checkout_request_id = models.CharField(max_length=10)
+    response_description = models.CharField(max_length=255)
+    response_code = models.CharField(max_length=10)
+    customer_message = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
